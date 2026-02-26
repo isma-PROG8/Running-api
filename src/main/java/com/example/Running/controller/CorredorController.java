565104a0974a28a4052adbf8b2addc6d0332a376
Isma.Prog8 <ismael30498@gmail.com>
2026-02-26 19:22:34 +0100

    Add CorredorController with CRUD endpoints

---
+++ src/main/java/com/example/Running/controller/CorredorController.java
@@ -0,0 +1,37 @@
+package com.example.Running.controller;
+
+
+import com.example.Running.model.Corredor;
+import com.example.Running.service.CorredorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/corredores")
+public class CorredorController {
+    @Autowired
+private CorredorService corredorService;
+    @GetMapping
+    public List<Corredor> obtenerCorredores(){
+        return corredorService.obtenerTodos();
+
+    }
+    @GetMapping("/{id}")
+    public Corredor buscarPorId(@PathVariable long id){
+        return corredorService.buscarPorId(id);
+    }
+    @PostMapping()
+    public void agregarCorredor(@RequestBody Corredor corredor){
+        corredorService.agregarCorredor(corredor);
+    }
+    @DeleteMapping("/{id}")
+    public void eliminarCorredor(@PathVariable long  id){
+        corredorService.eliminarCorredor(id);
+    }
+
+
+
+
+}
