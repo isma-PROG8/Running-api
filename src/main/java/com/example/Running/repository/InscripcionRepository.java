a5d6c6f1915980eb078a3faded7030587e8228d9
Isma.Prog8 <ismael30498@gmail.com>
2026-03-03 10:41:29 +0100

    Add business logic validations for inscription // añadiendo logica de negocio y validaciones de inscripciones

--- src/main/java/com/example/Running/repository/InscripcionRepository.java
+++ src/main/java/com/example/Running/repository/InscripcionRepository.java
@@ -1,11 +1,13 @@
 package com.example.Running.repository;
 
 import com.example.Running.model.Carrera;
+import com.example.Running.model.Corredor;
 import com.example.Running.model.Inscripcion;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface InscripcionRepository extends JpaRepository<Inscripcion, Long> {
     long countByCarrera(Carrera carrera);
-
-
+    boolean existsByCorredorAndCarrera(Corredor corredor, Carrera carrera);
 }
+
+
--- src/main/java/com/example/Running/service/InscripcionService.java
+++ src/main/java/com/example/Running/service/InscripcionService.java
@@ -1,8 +1,10 @@
 package com.example.Running.service;
 
 import com.example.Running.model.Carrera;
+import com.example.Running.model.Corredor;
 import com.example.Running.model.Inscripcion;
 import com.example.Running.repository.CarreraRepository;
+import com.example.Running.repository.CorredorRepository;
 import com.example.Running.repository.InscripcionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,6 +18,8 @@ public class InscripcionService {
     private CarreraRepository carreraRepository;
     @Autowired
     private InscripcionRepository inscripcionRepository;
+    @Autowired
+    private CorredorRepository corredorRepository;
 
     public List<Inscripcion> obtenerInscripciones() {
         return inscripcionRepository.findAll();
@@ -27,6 +31,16 @@ public class InscripcionService {
 
     public void crearInscripcion(Inscripcion inscripcion) {
         Carrera carrera = carreraRepository.findById(inscripcion.getCarrera().getId()).orElseThrow(()->new RuntimeException("No existe la carrera con id" +inscripcion.getCarrera().getId() ));
+        Corredor corredor = corredorRepository.findById(inscripcion.getCorredor().getId())
+                .orElseThrow(() -> new RuntimeException("No existe el corredor con id " + inscripcion.getCorredor().getId()));
+
+        if(carrera.getPlazasOcupadas()>=carrera.getPlazasMax()){
+            throw new RuntimeException("No quedan plazas disponibles");
+        }
+        if (inscripcionRepository.existsByCorredorAndCarrera(corredor,carrera)) {
+            throw new RuntimeException("El corredor ya esta inscrito en la carrera");
+        }
+
         long totalInscritos = inscripcionRepository.countByCarrera(carrera);
         inscripcion.setCarrera(carrera);
         inscripcion.setDorsal((int) totalInscritos + 1);
@@ -35,6 +49,7 @@ public class InscripcionService {
         carrera.setPlazasOcupadas(carrera.getPlazasOcupadas()+1);
         carreraRepository.save(carrera);
         inscripcionRepository.save(inscripcion);
+
     }
     public void eliminarInscripcion(long id) {
         inscripcionRepository.deleteById(id);
