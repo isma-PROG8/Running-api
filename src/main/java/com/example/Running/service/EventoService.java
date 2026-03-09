5815fb556ae0cd0abd4ebd843f19b25739e8a892
Isma.Prog8 <ismael30498@gmail.com>
2026-03-09 19:26:32 +0100

    changes eventoService for save Carreras and deleteMode

--- src/main/java/com/example/Running/service/EventoService.java
+++ src/main/java/com/example/Running/service/EventoService.java
@@ -1,6 +1,8 @@
 package com.example.Running.service;
 
+import com.example.Running.model.Carrera;
 import com.example.Running.model.Evento;
+import com.example.Running.repository.CarreraRepository;
 import com.example.Running.repository.EventoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -8,6 +10,8 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 @Service
 public class EventoService {
+    @Autowired
+    private CarreraRepository carreraRepository;
 
     @Autowired
     private EventoRepository eventoRepository;
@@ -19,9 +23,21 @@ public class EventoService {
         return eventoRepository.findById(id).orElseThrow(()-> new RuntimeException("No se encontro el evento con el id "+id));
     }
     public Evento guardarEvento(Evento evento){
-        return eventoRepository.save(evento);
+        Evento eventoGuardado = eventoRepository.save(evento);
+
+        if(evento.getCarreras() != null) {
+            for(Carrera carrera : evento.getCarreras()) {
+                carrera.setEvento(eventoGuardado);
+                carreraRepository.save(carrera);
+            }
+        }
+        return eventoGuardado;
     }
     public void eliminarEvento(long id){
+        Evento evento = eventoRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("No existe el evento con id " + id));
+
+        carreraRepository.deleteAll(evento.getCarreras());
         eventoRepository.deleteById(id);
     }
 }
